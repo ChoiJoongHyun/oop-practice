61e8167a918239c2fb2f27444e6c0847160fcaba
ChoiJoongHyun <joonghyun0117@gmail.com>
2018-01-18 13:42:10 +0900

    주석제거.

--- lotto/src/test/java/com/dotori/lotto/store/LottoStoreTest.java
+++ lotto/src/test/java/com/dotori/lotto/store/LottoStoreTest.java
@@ -7,20 +7,6 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-/* Copyright (c) 2018 ZUM Internet, Inc.
- * All right reserved.
- * http://www.zum.com
- * This software is the confidential and proprietary information of ZUM
- * , Inc. You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license agreement
- * you entered into with ZUM.
- *
- * Revision History
- * Author                    Date                     Description
- * ------------------       --------------            ------------------
- *   joonghyun                2018-01-18
- */
-
 public class LottoStoreTest {
 
     LottoStore lottoStore = new LottoStore();
